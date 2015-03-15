95150bcdb9ba70f783b7e7ec375a7e3bf6a6050a
sourabh <singhsourabh90@gmail.com>
2015-03-15 11:01:09 +0530

    no need to press enter anymore in basic setup

--- basic-setup.sh
+++ basic-setup.sh
@@ -2,39 +2,42 @@
 # Ubuntu Developer Script For Initial-Ubuntu-Setup
 # Created by Sourabh-Singh.
 #   chrome
-#   Apache An
-#   Android
-#   NPM
-#   Apache Cordova
-#   Ionic Framework
+#   sublime
+#   git
+#   terminator
+#   zsh
+#   oh-my-zsh
+#	variety
+#	pip
+#   postgress
 
 #chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update
-sudo apt-get install google-chrome-stable
+sudo apt-get install google-chrome-stable -y
 #sublime
-sudo add-apt-repository ppa:webupd8team/sublime-text-2
+sudo add-apt-repository ppa:webupd8team/sublime-text-2 -y
 sudo apt-get update
-sudo apt-get install sublime-text
+sudo apt-get install sublime-text -y
 #git
 sudo apt-get update
-sudo apt-get install git
+sudo apt-get install git -y
 #terminator
 sudo apt-get update
-sudo add-apt-repository ppa:gnome-terminator
+sudo add-apt-repository ppa:gnome-terminator -y
 sudo apt-get update
-sudo apt-get install terminator
+sudo apt-get install terminator -y
 #zsh
-sudo apt-get install zsh
+sudo apt-get install zsh -y
 #oh-my-zsh
 wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
 #variety
-sudo add-apt-repository ppa:peterlevi/ppa
+sudo add-apt-repository ppa:peterlevi/ppa -y
 sudo apt-get update
-sudo apt-get install variety
+sudo apt-get install variety -y
 #pip
-sudo apt-get install python-pip
+sudo apt-get install python-pip -y
 #postgres
 sudo apt-get update
-sudo apt-get install postgresql postgresql-contrib
+sudo apt-get install postgresql postgresql-contrib -y
