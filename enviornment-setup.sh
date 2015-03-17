f0ad46207bf1507eab9c7e6cedf133c54ab666dc
Amol Sharma <amolsharma99@gmail.com>
2015-03-17 12:25:57 +0530

    correct comment

--- enviornment-setup.sh
+++ enviornment-setup.sh
@@ -61,7 +61,7 @@ sudo apt-get update
 sudo apt-get install postgresql postgresql-contrib -y
 
 #tomcat
-sudo apt-get install tomcat7 #http://localhost:8080 should in case of success
+sudo apt-get install tomcat7 #http://localhost:8080 should work in case of success
 sudo apt-get install tomcat7-docs tomcat7-admin tomcat7-examples #additional packages
  
 echo "alias psqlre='sudo /etc/init.d/postgresql start'" >> ".zshrc"
