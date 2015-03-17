5d8620cc3ebdf3d1267a4013a956e26ddcd63629
1900joker <singhsourabh90@gmail.com>
2015-03-17 13:29:56 +0530

    added tomcat to instalation

--- enviornment-setup.sh
+++ enviornment-setup.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 # Ubuntu Developer Script For MIS ETS Setup
-# Created by hl_joker [SourabhSingh]
-#
-#
+# Contributors : Anmol , Sourabh 
 # Downloads and configures the following
 #
 #   Maven
 #   Activemq
-#	Memcache
-#	Postgres
+#   Memcache
+#   Postgres
 #   Tomcat
 
+#PATHS
 HOME_PATH=$(cd ~/ && pwd)
 INSTALL_PATH=/opt
 MAVEN_PATH=/opt/maven 
 ACTIVEMQ_PATH=/opt/activemq
 
-# x86_64 or i686
+#OS Arcitecture x86_64 or i686 
 LINUX_ARCH="$(lscpu | grep 'Architecture' | awk -F\: '{ print $2 }' | tr -d ' ')"
 
-# Latest Maven as of 15 March 2015
+# Latest Available tar.gz
 MAVEN="http://apache.bytenet.in/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz"
 ACTIVEMQ="http://archive.apache.org/dist/activemq/apache-activemq/5.9.0/apache-activemq-5.9.0-bin.tar.gz"
 
@@ -27,17 +26,28 @@ apt-get update
 
 cd ~/Desktop
 
+#maven
 wget "$MAVEN" -O "maven.tgz"
-wget "$ACTIVEMQ" -O "activemq.tgz"
-
 tar zxf "maven.tgz" -C "$INSTALL_PATH"
-tar zxf "activemq.tgz" -C "$INSTALL_PATH"
-
 cd "$INSTALL_PATH" && mv "apache-maven-3.2.5" "maven"
-cd "$INSTALL_PATH" && mv "apache-activemq-5.9.0" "activemq"
-
 cd "$INSTALL_PATH" && chown root:root "maven" -R
+
+#activemq
+wget "$ACTIVEMQ" -O "activemq.tgz"
+tar zxf "activemq.tgz" -C "$INSTALL_PATH"
+cd "$INSTALL_PATH" && mv "apache-activemq-5.9.0" "activemq"
 cd "$INSTALL_PATH" && chown root:root "activemq" -R
+
+#memcached
+sudo apt-get install memcached -y
+
+#postgres
+sudo apt-get update
+sudo apt-get install postgresql postgresql-contrib -y
+
+#tomcat
+sudo apt-get install tomcat7 -y #http://localhost:8080 should work in case of success
+sudo apt-get install tomcat7-docs tomcat7-admin tomcat7-examples -y #additional packages
 
 cd ~/
 
@@ -45,7 +55,7 @@ cd ~/
 echo "export PATH=\$PATH:$MAVEN_PATH/bin" >> ".profile"
 echo "export PATH=\$PATH:$ACTIVEMQ_PATH/bin" >> ".profile"
 
-#Add Path in zshrc file for zsh shell
+#Add Path in .zshrc file for zsh shell
 echo "export PATH=\$PATH:$MAVEN_PATH/bin" >> ".zshrc"
 echo "export PATH=\$PATH:$ACTIVEMQ_PATH/bin" >> ".zshrc"
 
@@ -54,15 +64,7 @@ export PATH=$PATH:$MAVEN_PATH/bin
 export PATH=$PATH:$ACTIVEMQ_PATH/bin
 
 
-sudo apt-get install memcached -y
-
-#postgres
-sudo apt-get update
-sudo apt-get install postgresql postgresql-contrib -y
-
-#tomcat
-sudo apt-get install tomcat7 #http://localhost:8080 should work in case of success
-sudo apt-get install tomcat7-docs tomcat7-admin tomcat7-examples #additional packages
+#Setting alias for usefull commands
  
 echo "alias psqlre='sudo /etc/init.d/postgresql start'" >> ".zshrc"
 echo "alias memcre='sudo /etc/init.d/memcached restart'" >> ".zshrc"
